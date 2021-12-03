1be2fd99e6ea845d2473812164191230fb4e1b52
SamShekhovtsov <semshehovtsov@gmail.com>
2021-12-03 22:24:28 +0200

    Angular 13 title changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.scss' ]
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent  {
-  name = 'Angular 6';
+export class AppComponent {
+  name = 'Angular 13';
 }
